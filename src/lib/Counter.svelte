dd7a0fff22b67a9e627aaf320006b38a6ed24c30
panutat.p <pp.panutat@gmail.com>
2025-01-13 02:28:04 +0700

    feat: button css

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -8,3 +8,18 @@
 <button onclick={increment}>
   count is {count}
 </button>
+
+<style>
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 2em;
+    font-weight: 500;
+    font-family: inherit;
+    cursor: pointer;
+  }
+  button:hover {
+    border-color: #646cff;
+  }
+</style>
