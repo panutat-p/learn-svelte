8a41c7ab4aace3b218b5e1514295d388562660d7
panutat.p <panutat.pree@kkpfg.com>
2025-01-13 02:14:41 +0700

    feat: alias import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
